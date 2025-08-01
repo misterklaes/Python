a3755af358dc089e0b62d05862c9f013042add38
misterklaes <maximleluk@google.com>
2025-08-01 13:58:32 +0300

    List item move from last to first position

---
+++ list_last_to_first.py
@@ -0,0 +1,6 @@
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  
+if len(lst) >= 1 and bool(lst) == True:
+    lst.insert(0, lst.pop())
+    print(lst)  # [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+else:
+    print("Список пустий")
