fffb7d3c00da53fa858cc3f98b2d6d1d2c401af3
misterklaes <maximleluk@google.com>
2025-08-01 14:04:54 +0300

    Доповнив текстове повідомлення у випадку малого списку та додав вивід первинного (незміненого) списку

--- list_last_to_first.py
+++ list_last_to_first.py
@@ -1,6 +1,7 @@
-lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  
-if len(lst) >= 1 and bool(lst) == True:
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+if len(lst) > 1 and bool(lst) == True:
     lst.insert(0, lst.pop())
     print(lst)  # [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
 else:
-    print("Список пустий")
+    print("Список пустий або надто малий для переміщення елементів.")
+    print(lst)  # [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
